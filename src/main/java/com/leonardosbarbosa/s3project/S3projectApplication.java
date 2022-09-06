4556915a18d41646d308e650e0f7f2f86b3565fa
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2022-09-05 22:04:43 -0300

    Initial config, S3Service

---
+++ src/main/java/com/leonardosbarbosa/s3project/S3projectApplication.java
@@ -0,0 +1,13 @@
+package com.leonardosbarbosa.s3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardosbarbosa/s3project/S3projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardosbarbosa.s3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
